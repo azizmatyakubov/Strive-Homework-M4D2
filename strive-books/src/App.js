3d16635c7c4c7622b0361df7865bf09c3249c681
AZIZ <a.matyaqubov1471@gmail.com>
2022-03-15 16:26:37 +0100

    add MyNav component

---
+++ strive-books/src/App.js
@@ -0,0 +1,10 @@
+import MyNav from "./components/MyNav";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <MyNav />
+  );
+}
+
+export default App;
---
+++ strive-books/src/components/MyNav.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Navbar, Container, Nav } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+function MyNav() {
+  return (
+      <>
+       <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+      </>
+  )
+}
+
+export default MyNav
